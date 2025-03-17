73e64823b73760e279a5619b0a5c5e744b3ecc92
Utkarsh-Anandani <utkarshanandani9@gmail.com>
2025-03-18 00:56:36 +0530

    Updated predict route to use s3 url

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.models import load_model
 import cv2
 import numpy as np
+import requests
 from flask import Flask, request, jsonify
 
 model = load_model('my_skin_disease_pred_model.h5')
@@ -18,42 +19,45 @@ class_names = {
 
 app = Flask(__name__)
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        file = request.files['image']
-        predicted_class_index, predicted_class_name, predicted_prob, image = predict_image(file)
+        data = request.json
+        print(data)
+        file_url = data.get('image')
 
-        res = {
-            'predicted_class': predicted_class_name,
-            'prediction_probability': str(predicted_prob)
-        }
+        if not file_url:
+            return jsonify({'error': 'No file URL provided'}), 400
 
-        return jsonify(res)
+        try:
+            predicted_class_index, predicted_class_name, predicted_prob = predict_image_from_url(file_url)
+            res = {
+                'predicted_class': predicted_class_name,
+                'prediction_probability': str(predicted_prob)
+            }
+            return jsonify(res)
 
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
 
-def predict_image(file):
-    # Read image
-    file_bytes = np.fromfile(file, np.uint8)
+def predict_image_from_url(file_url):
+    response = requests.get(file_url)
+    if response.status_code != 200:
+        raise Exception(f"Failed to fetch image from URL: {file_url}")
+
+    file_bytes = np.frombuffer(response.content, np.uint8)
     image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
-    # Preprocess image (resize and normalize)
     image = cv2.resize(image, (64, 64))
     image = image.astype('float32') / 255.0
-
     image = np.expand_dims(image, axis=0)
 
     predictions = model.predict(image)
-    print(predictions)
-
     predicted_class_index = np.argmax(predictions[0])
     predicted_prob = predictions[0][predicted_class_index]
     predicted_class_name = class_names[predicted_class_index]
 
-    return predicted_class_index, predicted_class_name, predicted_prob, image[0]
-
-
+    return predicted_class_index, predicted_class_name, predicted_prob
 
 if __name__ == '__main__':
     app.run(port=5000)
